db0806bea35c2c7bc5a8bf9d03229fa4599ffd8b
SohailLapi <msohail9x@gmail.com>
2022-10-05 00:05:02 +0500

    safd

---
+++ Question19.js
@@ -0,0 +1,8 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+
+
+guestlist=["Ali","Atif","Asif"]
+
+console.log("total person's invited are : "+guestlist.length)
+
