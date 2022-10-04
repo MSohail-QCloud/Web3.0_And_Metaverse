19774e1fc50415b86fa124ef3d2913a676f30fd6
Muhammad Sohail -ofc <msohail9x@gmail.com>
2022-10-04 14:06:25 +0500

    sdfs

---
+++ Question7.js
@@ -0,0 +1,11 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+
+addition8=4+4
+console.log("Result of addition 4+4="+addition8)
+subtraction=12-4
+console.log("Result of addition 12-4="+subtraction)
+Multiplication = 4*2
+console.log("Result of Multipley 4*2="+Multiplication)
+division=64/8
+console.log("Result of Division 64/8="+division)
