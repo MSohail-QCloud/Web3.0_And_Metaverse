ba1f23b460615ee29f736a1ad399b5db2d9e549f
SohailLapi <msohail9x@gmail.com>
2022-10-15 19:53:43 +0500

    Type script added

---
+++ Javascript Exercises 45 Qustions/Question38.js
@@ -0,0 +1,10 @@
+// // Cities: Write a function called describe_city() that accepts the name of a city and its country. 
+// The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for 
+// the country a default value. Call your function for three different cities, at least one of which is 
+// not in the default country.
+
+function describe_city(city, country) {
+    console.log("This shirt size is "+ size +" and printed on is " +Statement)
+  }
+  
+  describe_city(17, "God is Greate")
