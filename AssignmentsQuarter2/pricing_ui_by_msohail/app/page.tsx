e5cc4a454c6d5fd2f6f1ebe0308367e15ed792f0
Muhammad Sohail <msohail9x@gmail.com>
2023-02-07 23:24:15 +0500

    adding assignment 2

---
+++ AssignmentsQuarter2/pricing_ui_by_msohail/app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+// import Image from "next/image";
+// import { Inter } from "@next/font/google";
+// import styles from "./page.module.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+import { ChakraProvider, Heading, Text } from "@chakra-ui/react";
+import Features from "./feature";
+import Header from "./Header";
+import Pricing from "./Pricing";
+
+export default function Home() {
+  return (
+    <ChakraProvider>
+      <Header />
+      <Pricing />
+      <Features />
+    </ChakraProvider>
+  );
+}
